5350099d69cea1700a0f36dbd144fb483028880a
srishti132 <srishtirastogi1208.work@gmail.com>
2026-07-29 00:13:56 +0530

    initial commit- basic HTML and CSS

---
+++ password generator/index.js
@@ -0,0 +1,19 @@
+let password = document.getElementById("password-btn")
+
+password.addEventListener('click',function(){
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let generatepassword = ""
+    
+    for(let i=0 ; i< 15; i++){
+         /*  let randomNumber =  Math.floor( Math.random()*14) + 1 */ 
+         // common mistake this will only pick up the characters upto first 14 characters from characters variable never the random characters that you need 
+
+         //better approach 
+         let randomNumber = Math.floor(Math.random() * characters.length);
+         //will help to select the random character 
+         generatepassword += characters[randomNumber]
+        
+    }
+     console.log(generatepassword)
+})
