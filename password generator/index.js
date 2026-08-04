3fcf8f716ddb2205ee37253aa8152eb6956bdd5d
srishti132 <srishtirastogi1208.work@gmail.com>
2026-08-04 23:51:09 +0530

    display of random password as placeholder
    random password generation in the placeholder and changed the styling

--- password generator/index.js
+++ password generator/index.js
@@ -15,6 +15,23 @@ password.addEventListener('click',function(){
          generatepassword += characters[randomNumber]
         
     }
-    input.value = generatepassword
-     console.log(generatepassword)
+   
+//     input.value = generatepassword
+      console.log(generatepassword)
+});
+ input.addEventListener("click", function () {
+ const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let generatepassword = ""
+    
+    for(let i=0 ; i< 15; i++){
+         /*  let randomNumber =  Math.floor( Math.random()*14) + 1 */ 
+         // common mistake this will only pick up the characters upto first 14 characters from characters variable never the random characters that you need 
+
+         //better approach 
+         let randomNumber = Math.floor(Math.random() * characters.length);
+         //will help to select the random character 
+         generatepassword += characters[randomNumber]
+   input.placeholder = generatepassword
+    }
 });
